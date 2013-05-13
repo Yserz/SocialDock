11cfb7dd7bd8e830e5e2639e09f256bfa4905bd2
MacYser <MacYser@i-wlan594.fh-brandenburg.de>
2013-05-13 12:44:24 +0200

    - cleanup

---
+++ SDAdminWeb/src/main/java/de/fhb/sd/adminweb/vaadin/Jee6UIProvider.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fhb.sd.adminweb.vaadin;
+
+import com.vaadin.server.UIClassSelectionEvent;
+import com.vaadin.server.UICreateEvent;
+import com.vaadin.server.UIProvider;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+/**
+ *
+ * @author MacYser
+ */
+public class Jee6UIProvider extends UIProvider {
+
+	@Override
+	public UI createInstance(UICreateEvent event) {
+		return ((Jee6VaadinServlet) VaadinServlet.getCurrent()).getUI();
+	}
+
+	@Override
+	public Class<? extends UI> getUIClass(UIClassSelectionEvent event) {
+		return ((Jee6VaadinServlet) VaadinServlet.getCurrent()).getUI().getClass();
+	}
+}
