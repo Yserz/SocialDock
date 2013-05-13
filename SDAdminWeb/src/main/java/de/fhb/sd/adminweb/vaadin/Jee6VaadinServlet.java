6b5da02479b2ceb31e74c11c88f253b2ae4a3d2c
Yserz <michael.koppen@googlemail.com>
2013-05-13 12:44:24 +0200

    - cleanup

---
+++ SDAdminWeb/src/main/java/de/fhb/sd/adminweb/vaadin/Jee6VaadinServlet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fhb.sd.adminweb.vaadin;
+
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import javax.inject.Inject;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ *
+ * @author MacYser
+ */
+@WebServlet(urlPatterns = "/*",
+		initParams = {
+	@WebInitParam(name = "UIProvider", value = "de.fhb.sd.adminweb.vaadin.Jee6UIProvider")})
+public class Jee6VaadinServlet extends VaadinServlet {
+
+	@Inject
+	private UI ui;
+
+	public UI getUI() {
+		return ui;
+	}
+}
---
+++ SDAdminWeb/src/main/webapp/VAADIN/widgetsets/com.vaadin.DefaultWidgetSet/deferredjs/7B70CBE9BBA9EB8EF818971FFAC3F7F0/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_vaadin_DefaultWidgetSet.runAsyncCallback1('Crb(2385,1,wLe);_.$b=function iud(){IXb((!DXb&&(DXb=new KXb),DXb),this.b.e)};hMe(gh)(1);\n//@ sourceURL=1.js\n')
