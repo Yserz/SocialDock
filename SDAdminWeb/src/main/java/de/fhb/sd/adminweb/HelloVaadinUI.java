ed02366934b4b69c46c9b878fcadd1ddcadefd85
Yserz <michael.koppen@googlemail.com>
2013-05-13 20:41:53 +0200

    - added copy script in maven 4 autodeploy - edited adminWeb bundle

--- SDAdminWeb/src/main/java/de/fhb/sd/adminweb/HelloVaadinUI.java
+++ SDAdminWeb/src/main/java/de/fhb/sd/adminweb/HelloVaadinUI.java
@@ -7,10 +7,15 @@ package de.fhb.sd.adminweb;
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Tree;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.VerticalSplitPanel;
 import de.fhb.sd.api.kernel.KernelServiceLocal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -31,6 +36,9 @@ public class HelloVaadinUI extends UI {
 	@OSGiService(dynamic = true)
 	KernelServiceLocal kernel;
 	private Table bundleList = new Table();
+	private MenuBar menu = new MenuBar();
+	private Tree tree = new Tree();
+	private Label detail = new Label("Detail");
 
 	/*
 	 * After UI class is created, init() is executed. You should build and wire
@@ -38,6 +46,52 @@ public class HelloVaadinUI extends UI {
 	 */
 	@Override
 	protected void init(VaadinRequest request) {
+		HorizontalSplitPanel horizontal = new HorizontalSplitPanel();
+		VerticalSplitPanel vertical = new VerticalSplitPanel();
+		VerticalSplitPanel content = new VerticalSplitPanel();
+
+		vertical.setSizeFull();
+		vertical.setSplitPosition(3, Unit.PERCENTAGE);
+		vertical.setLocked(true);
+		horizontal.setSizeFull();
+		horizontal.setSplitPosition(20, Unit.PERCENTAGE);
+
+		content.setSizeFull();
+		content.setSplitPosition(80, Unit.PERCENTAGE);
+
+		initMenu();
+		initTable();
+		initTree();
+
+
+		vertical.addComponent(menu);
+		vertical.addComponent(horizontal);
+
+		horizontal.addComponent(tree);
+		horizontal.addComponent(content);
+
+		content.addComponent(bundleList);
+		content.addComponent(detail);
+
+		setContent(vertical);
+	}
+
+	public void initMenu() {
+		menu.setSizeFull();
+		menu.addItem("test", new MenuBar.Command() {
+			@Override
+			public void menuSelected(MenuBar.MenuItem selectedItem) {
+				throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+			}
+		});
+	}
+
+	public void initTree() {
+		tree.addItem("Bla");
+		tree.setHeight(100, Unit.PERCENTAGE);
+	}
+
+	public void initTable() {
 		SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
 		bundleList.addContainerProperty("Name", String.class, null);
 		bundleList.addContainerProperty("ID", String.class, null);
@@ -62,7 +116,6 @@ public class HelloVaadinUI extends UI {
 			},
 					bundle);
 		}
-		setContent(bundleList);
-		bundleList.setSizeFull();
+		bundleList.setHeight(100, Unit.PERCENTAGE);
 	}
 }
