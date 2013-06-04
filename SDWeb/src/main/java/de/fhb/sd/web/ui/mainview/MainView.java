fdc9f9a8dcd57e24bd47d006bbf490d6411bac73
Kaain <ott@fh-brandenburg.de>
2013-06-04 14:29:03 +0200

    edited imports

--- SDWeb/src/main/java/de/fhb/sd/web/ui/mainview/MainView.java
+++ SDWeb/src/main/java/de/fhb/sd/web/ui/mainview/MainView.java
@@ -9,25 +9,25 @@ import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalSplitPanel;
+import de.fhb.sd.api.nyt.NewYorkTimesLocal;
 import de.fhb.sd.api.twitter.TwitterLocal;
-import de.fhb.sd.web.ui.mainview.component.TopMenuBar;
 import de.fhb.sd.web.ui.mainview.component.MessageTableWithDetails;
+import de.fhb.sd.web.ui.mainview.component.TopMenuBar;
 
 /**
- *
  * @author MacYser
  */
 public class MainView extends CustomComponent implements View {
 
 	private TwitterLocal twitter;
-	private de.fhb.sd.api.nyt.NewYorkTimesLocal nyt;
+	private NewYorkTimesLocal nyt;
 	/* define Layout objects */
 	private VerticalSplitPanel vertical = new VerticalSplitPanel();
 	/* define Components */
 	private MessageTableWithDetails content;
 	private TopMenuBar topMenuBar;
 
-	public MainView(final TwitterLocal twitter, final de.fhb.sd.api.nyt.NewYorkTimesLocal nyt) {
+	public MainView(final TwitterLocal twitter, final NewYorkTimesLocal nyt) {
 		super();
 		this.twitter = twitter;
 		this.nyt = nyt;
