0c764a4770a9bc89fd0ad4ee4dd311fcb0bb6bee
Kaain <ott@fh-brandenburg.de>
2013-06-11 17:41:03 +0200

    deleted some dead code

--- SDWeb/src/main/java/de/fhb/sd/web/ui/golem/component/GolemDetailPanel.java
+++ SDWeb/src/main/java/de/fhb/sd/web/ui/golem/component/GolemDetailPanel.java
@@ -12,50 +12,6 @@ public class GolemDetailPanel extends DetailPanel {
 		super(selectedMessage);
 	}
 
-//	@Override
-//	protected Panel addDetailContent() {
-//		Panel panel = new Panel();
-//		panel.setSizeFull();
-//
-//		GridLayout infoGrid = new GridLayout(2, 6);
-//
-//		if (selectedMessage != null) {
-//			GolemMessage golemM = (GolemMessage) selectedMessage;
-//			if (!golemM.getImageURLMedium().isEmpty()) {
-//				ExternalResource resource = new ExternalResource(golemM.getImageURLMedium());
-//				Image img = new Image();
-//				img.setSource(resource);
-//                infoGrid.addComponent(img, 0,0,1,0);
-//			}
-//
-//            Label component = new Label("Title: ");
-//            component.setWidth(100, Unit.PIXELS);
-//            infoGrid.addComponent(component,0,1);
-//			Label titleLabel = new Label(golemM.getTitle());
-//			infoGrid.addComponent(titleLabel,1,1);
-//
-//			infoGrid.addComponent(new Label("Author: "),0,2);
-//			Label authorLabel = new Label(golemM.getAuthor());
-//			infoGrid.addComponent(authorLabel,1,2);
-//
-//			infoGrid.addComponent(new Label("Published: "),0,3);
-//			Label publishedLabel = new Label(golemM.getPublished() + "");
-//			infoGrid.addComponent(publishedLabel,1,3);
-//
-//			infoGrid.addComponent(new Label("Message: "),0,4);
-//			Label messageLabel = new Label(golemM.getMessage());
-//			infoGrid.addComponent(messageLabel,1,4);
-//
-//            ExternalResource linkResource = new ExternalResource(golemM.getURL());
-//            infoGrid.addComponent(new Label("URL: "),0,5);
-//            Link urlLink = new Link(golemM.getURL(), linkResource);
-//            infoGrid.addComponent(urlLink,1,5);
-//		}
-//
-//		panel.setContent(infoGrid);
-//		return panel;
-//	}
-
     @Override
     protected Panel addDetailContent() {
         Panel panel = new Panel();
